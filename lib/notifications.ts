5f598c7f7e7994a74a6b0a8423551df0b51121eb
Suman Sourabh <sumsourabh14@gmail.com>
2025-03-03 02:12:26 +0530

    added expo file system

--- lib/notifications.ts
+++ lib/notifications.ts
@@ -44,7 +44,7 @@ export async function registerForPushNotificationsAsync() {
           projectId,
         })
       ).data;
-      console.log(pushTokenString);
+      // console.log(pushTokenString);
       return pushTokenString;
     } catch (e: unknown) {
       handleRegistrationError(`${e}`);
